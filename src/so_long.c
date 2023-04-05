48a6c75e24f994dbcdcd8925860827345e7a0712
rdragan <rdragan@student.42heilbronn.de>
2023-04-05 23:59:41 +0200

    feature/function-to-handle-errors/handle_error

--- src/so_long.c
+++ src/so_long.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include "so_long.h"
 
-int	main(void)
+void	handle_error(char *str)
 {
+	ft_putstr_fd("Error: ", STDERR_FILENO);
+	ft_putstr_fd(str, STDERR_FILENO);
+	ft_putchar_fd('\n', STDERR_FILENO);
+	exit(1);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc != 2)
+		handle_error("Invalid number of arguments!");
+	printf("%s", argv[1]);
 	return (0);
 }
