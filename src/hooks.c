78e7bf232926b1b6c9b2d8b198505d84a03b30fa
rdragan <rdragan@student.42heilbronn.de>
2023-04-07 21:40:26 +0200

    fix-bug/close-window-on-esc

--- src/hooks.c
+++ src/hooks.c
@@ -2,33 +2,33 @@
 
 void	get_keystroke_hook(mlx_key_data_t keydata, void *param)
 {
-	t_map	*map;
+	t_keyhook	*data;
 
-	map = param;
+	data = (t_keyhook *) param;
 	if (keydata.key == MLX_KEY_ESCAPE && keydata.action == MLX_PRESS)
-		puts("ESCAPE!");
-	if (keydata.key == MLX_KEY_W && keydata.action == MLX_PRESS && map->p_row > 1)
+		mlx_close_window(data->mlx);
+	if (keydata.key == MLX_KEY_W && keydata.action == MLX_PRESS && data->map->p_row > 1)
 	{
-		map->p_row -= 1;
-		ft_putnbr_fd(map->p_row, 1);
+		data->map->p_row -= 1;
+		ft_putnbr_fd(data->map->p_row, 1);
 		ft_putchar_fd('\n', 1);
 	}
-	if (keydata.key == MLX_KEY_S && keydata.action == MLX_PRESS && map->p_row < map->height - 2)
+	if (keydata.key == MLX_KEY_S && keydata.action == MLX_PRESS && data->map->p_row < data->map->height - 2)
 	{
-		map->p_row += 1;
-		ft_putnbr_fd(map->p_row, 1);
+		data->map->p_row += 1;
+		ft_putnbr_fd(data->map->p_row, 1);
 		ft_putchar_fd('\n', 1);
 	}
-	if (keydata.key == MLX_KEY_A && keydata.action == MLX_PRESS && map->p_col > 1)
+	if (keydata.key == MLX_KEY_A && keydata.action == MLX_PRESS && data->map->p_col > 1)
 	{
-		map->p_col -= 1;
-		ft_putnbr_fd(map->p_col, 1);
+		data->map->p_col -= 1;
+		ft_putnbr_fd(data->map->p_col, 1);
 		ft_putchar_fd('\n', 1);
 	}
-	if (keydata.key == MLX_KEY_D && keydata.action == MLX_PRESS && map->p_col < map->width - 2)
+	if (keydata.key == MLX_KEY_D && keydata.action == MLX_PRESS && data->map->p_col < data->map->width - 2)
 	{
-		map->p_col += 1;
-		ft_putnbr_fd(map->p_col, 1);
+		data->map->p_col += 1;
+		ft_putnbr_fd(data->map->p_col, 1);
 		ft_putchar_fd('\n', 1);
 	}
 }
