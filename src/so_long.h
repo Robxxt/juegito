fb3cbd84c39e61cc64c504e96d68a24e0fea8429
rdragan <rdragan@student.42heilbronn.de>
2023-04-09 11:19:04 +0200

    feature/new-struct
    Create t_texture struct

--- src/so_long.h
+++ src/so_long.h
@@ -20,6 +20,21 @@ typedef struct s_map
 	int		e_col;
 } t_map;
 
+typedef struct s_keyhook
+{
+	t_map	*map;
+	mlx_t	*mlx;
+} t_keyhook;
+
+typedef struct	s_texture
+{
+	mlx_texture_t	*main_char;
+	mlx_texture_t	*comestible;
+	mlx_texture_t	*exit;
+	mlx_texture_t	*walls;
+} t_texture;
+
+
 void	handle_error(char *str);
 void	file_checker(char *filename);
 t_map	*get_map(char *filename);
@@ -27,5 +42,6 @@ void	check_map(t_map *map, char *filename);
 void	verify_valid_path(t_map *map, char *filename);
 int		is_perimeter(int y, int x, int map_height, int map_width);
 void	get_keystroke_hook(mlx_key_data_t keydata, void *param);
+void	display_window(t_map *map);
 
 #endif
