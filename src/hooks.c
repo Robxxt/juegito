b98151fd178f5c43708afa66f6a1210809e3e46f
rdragan <rdragan@student.42heilbronn.de>
2023-04-10 14:23:34 +0200

    finish-game

--- src/hooks.c
+++ src/hooks.c
@@ -6,11 +6,13 @@
 /*   By: rdragan <rdragan@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/09 18:22:43 by rdragan           #+#    #+#             */
-/*   Updated: 2023/04/10 14:13:15 by rdragan          ###   ########.fr       */
+/*   Updated: 2023/04/10 14:22:59 by rdragan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+
+
 
 void	move_char_horizontally(mlx_t *mlx, t_map *map, t_image *img, int dir)
 {
@@ -25,17 +27,25 @@ void	move_char_horizontally(mlx_t *mlx, t_map *map, t_image *img, int dir)
 		{
 			map->comestible_achieved++;
 			map->matrix[y][x + dir] = '0';
-			ft_putstr_fd("GOT COMESTIBLE!", 1);
+			ft_putstr_fd("GOT COMESTIBLE!\n", 1);
 		}
-		mlx_image_to_window(mlx, img->floor,
+		if (map->matrix[y][x + dir] == 'E' && map->comestible_achieved == map->comestibles)
+		{
+			ft_putstr_fd("YOU WIN!\n", 1);
+			mlx_close_window(mlx);
+		}
+		else if (map->matrix[y][x + dir] != 'E')
+		{
+			mlx_image_to_window(mlx, img->floor,
 			map->p_col * 32, map->p_row * 32);
-		map->p_col = x + dir;
-		mlx_image_to_window(mlx, img->main_char,
-			map->p_col * 32, map->p_row * 32);
-		if (dir == -1)
-			ft_putstr_fd("<LEFT", 1);
-		else
-			ft_putstr_fd("RIGHT>", 1);
+			map->p_col = x + dir;
+			mlx_image_to_window(mlx, img->main_char,
+				map->p_col * 32, map->p_row * 32);
+			if (dir == -1)
+				ft_putstr_fd("<LEFT\n", 1);
+			else
+				ft_putstr_fd("RIGHT>\n", 1);
+		}
 	}
 }
 
@@ -52,16 +62,24 @@ void	move_char_vertically(mlx_t *mlx, t_map *map, t_image *img, int dir)
 		{
 			map->comestible_achieved++;
 			map->matrix[y + dir][x] = '0';
-			ft_putstr_fd("GOT COMESTIBLE!", 1);
+			ft_putstr_fd("GOT COMESTIBLE!\n", 1);
 		}
-		mlx_image_to_window(mlx, img->floor, map->p_col * 32, map->p_row * 32);
-		map->p_row = y + dir;
-		mlx_image_to_window(mlx, img->main_char,
-			map->p_col * 32, map->p_row * 32);
-		if (dir == -1)
-			ft_putstr_fd("UP^", 1);
-		else
-			ft_putstr_fd("DOWN", 1);
+		if (map->matrix[y + dir][x] == 'E' && map->comestible_achieved == map->comestibles)
+		{
+			mlx_close_window(mlx);
+			ft_putstr_fd("YOU WIN!\n", 1);
+		}
+		else if (map->matrix[y + dir][x] != 'E')
+		{
+			mlx_image_to_window(mlx, img->floor, map->p_col * 32, map->p_row * 32);
+			map->p_row = y + dir;
+			mlx_image_to_window(mlx, img->main_char,
+				map->p_col * 32, map->p_row * 32);
+			if (dir == -1)
+				ft_putstr_fd("UP^\n", 1);
+			else
+				ft_putstr_fd("DOWN\n", 1);
+		}
 	}
 }
 
